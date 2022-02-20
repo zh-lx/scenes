89f5b4eb96210acb985d997b0d3f940cc5a6341c
zh-lx <18366276315@163.com>
2022-02-20 12:30:55 +0800

    arco-cli: initialize project

---
+++ config-overrides.js
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const {
+  override,
+  addWebpackModuleRule,
+  addWebpackPlugin,
+  addWebpackAlias,
+} = require('customize-cra');
+const ArcoWebpackPlugin = require('@arco-design/webpack-plugin');
+const addLessLoader = require('customize-cra-less-loader');
+
+module.exports = {
+  webpack: override(
+    addLessLoader(),
+    addWebpackModuleRule({
+      test: /\.svg$/,
+      loader: '@svgr/webpack',
+    }),
+    addWebpackPlugin(
+      new ArcoWebpackPlugin({
+        theme: '@arco-themes/react-arco-pro',
+      })
+    ),
+    addWebpackAlias({
+      '@': path.resolve(__dirname, 'src'),
+    })
+  ),
+};
